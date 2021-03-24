0e2a3a97548f868e0105ff5f5c4d45be5ad43cc3
tbnsok40 <tbnsok40@naver.com>
2021-03-24 20:37:11 +0900

    lombok

--- src/main/java/spring/core/order/OrderServiceImpl.java
+++ src/main/java/spring/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package spring.core.order;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import spring.core.discount.DiscountPolicy;
@@ -9,7 +10,10 @@ import spring.core.member.Member;
 import spring.core.member.MemberRepository;
 import spring.core.member.MemoryMemberRepository;
 
+
+// 정리: 최근에는 생성자를 딱 1개를 두고, lombok 라이브러리를 사용하여 코드를 깔끔하게 만드는게 트랜드
 @Component
+@RequiredArgsConstructor // 생성자를 지워벌인다..!!! 개신기하네 --> requiredArgs: final keyword가 붙은 필드, Constructor: 생성자 / 임의의 필드(final)가 추가되는 상황에서 매우 편리하다(자동주입)
 public class OrderServiceImpl implements OrderService {
 
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //고정 할인이 아닌 rateDiscount로 바꾸고 싶으면, new 이하 부분만 갈아끼우면 된다.
@@ -18,19 +22,18 @@ public class OrderServiceImpl implements OrderService {
     // 추상 (인터페이스) 뿐만 아니라 구체(구현) 클래스에도 의존하고 있다. (fixdiscountpolicy, ratediscountpolicy) --> DIP, OCP 위반
     // 클라이언트 코드에 손대서 문제인 것..?
 
-
+    // final keyword가 붙으면 필수값이 된다.
     // field -> 수정하려면 final 빼고 setter 사용하여 수정한다 => @Autowired 붙여줘야함
     private final DiscountPolicy discountPolicy; // 이렇게 바꾼다 (DIP는지킨 상태) => 이 상태면 nullpointerException
 //    private final MemberRepository memberRepository = new MemoryMemberRepository(); // 마찬가지로, 저장소(db)를 바꾸고 싶으면, new 이하만 바꾸면 된다. //AppConfig에 등록했으니 지워준다
     private final MemberRepository memberRepository;
 
     // 생성자엔 null 할당 X (관례상)
-    @Autowired // 생성자에 @autowired 어노테이션 붙인다.
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { // 철저히 인터페이스에만 의존하고 있다.
-
-        this.discountPolicy = discountPolicy;
-        this.memberRepository = memberRepository;
-    }
+//    @Autowired // 생성자에 @autowired 어노테이션 붙인다. -> 생성자 하나일 때는 autowired 생략 가능
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { // 철저히 인터페이스에만 의존하고 있다.
+//        this.discountPolicy = discountPolicy;
+//        this.memberRepository = memberRepository;
+//    }
     // 생성자가 한개만 있을 땐, @Autowired를 생략해도 자동주입된다.
 
     @Override
